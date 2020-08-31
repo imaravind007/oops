ddc2dc6eccc3a78eb02f3c0d8a38359846b699d2
Aravinthan <aramanathan2@hawk.iit.edu>
2020-08-31 09:20:30 -0500

    Q1 passed

--- prj01.py
+++ prj01.py
@@ -4,39 +4,60 @@ import easynn as nn
 # Create a numpy array of 10 rows and 5 columns.
 # Set the element at row i and column j to be i+j.
 def Q1():
-    return None
-
+    a = np.zeros([10,5])
+    for i in range(10):
+        for j in range(5):
+            a[i][j] = i+j
+    return a
 # Add two numpy arrays together.
 def Q2(a, b):
-    return None
+    return a+b
 
 # Multiply two 2D numpy arrays using matrix multiplication.
 def Q3(a, b):
-    return None
+    c = [0]
+    c = np.dot(a,b)
+    return c
+
 
 # For each row of a 2D numpy array, find the column index
 # with the maximum element. Return all these column indices.
 def Q4(a):
-    return None
+    arr = np.array(a)
+    max_index = np.argmax(arr,axis=1)
+    return max_index
 
 # Solve Ax = b.
 def Q5(A, b):
-    return None
+    x = np.linalg.solve(A,b)
+    return x
 
 # Return an EasyNN expression for a+b.
 def Q6():
-    return None
+    a = nn.Input("a")
+    b = nn.Input("b")
+    c = a + b
+    return c
 
 # Return an EasyNN expression for a+b*c.
 def Q7():
-    return None
+    a = nn.Input("a")
+    b = nn.Input("b")
+    c = nn.Input("c")
+    d = a + b*c
+    return d
 
 # Given A and b, return an EasyNN expression for Ax+b.
 def Q8(A, b):
-    return None
+    a = nn.Input("A")
+    b = nn.Input("x")
+    c = nn.Input("b")
+    d = a * b + c
+    return d
 
 # Given n, return an EasyNN expression for x**n.
 def Q9(n):
+    
     return None
 
 # Return an EasyNN expression to compute
